245a2dc5f8e82f299a3066ee7c36358daca25484
Pantho <panthohaque927908@gmail.com>
2023-03-22 14:48:01 +0600

    conflict file created

---
+++ app/src/main/java/com/farizma/truthdare/conflict.java
@@ -0,0 +1,36 @@
+package com.farizma.truthdare;
+
+public class conflict {
+    int roll;
+    String id,name;
+
+    public conflict(int roll, String id, String name) {
+        this.roll = roll;
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
