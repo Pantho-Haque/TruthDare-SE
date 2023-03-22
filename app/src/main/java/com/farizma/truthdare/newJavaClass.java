6ec0bee8eb8e982d149fd3a18221f19eeaff4f04
Pantho <panthohaque927908@gmail.com>
2023-03-22 14:40:01 +0600

    newJavaClass added

---
+++ app/src/main/java/com/farizma/truthdare/newJavaClass.java
@@ -0,0 +1,27 @@
+package com.farizma.truthdare;
+
+public class newJavaClass {
+    int roll;
+    String name;
+
+    public newJavaClass(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
